39d08b1cb80f6cfe30a4ec4e242307b7b92f9a31
Ben Carew <bencarew88@gmail.com>
2023-11-29 16:19:39 +0100

    Tidy python code

--- Spotify.py
+++ Spotify.py
@@ -15,44 +15,54 @@ df.head()
 df.info()
 df.describe()
 
-correlation_matrix = df.corr(numeric_only=True)
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-plt.title('Correlation Matrix Heatmap')
-plt.show()
+def CorrMat(df):
+    correlation_matrix = df.corr(numeric_only=True)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+    plt.title('Correlation Matrix Heatmap')
+    plt.show()
 
-pd.plotting.scatter_matrix(df);
+def ScatterMatrix(df):
+    pd.plotting.scatter_matrix(df);
+    # plt.show()
 
-x = df.select_dtypes(include='number').values
-scaler = pp.MinMaxScaler()
-x_scaled = scaler.fit_transform(x)
-dfnorm = pd.DataFrame(x_scaled,columns = df.select_dtypes(include='number').columns)
-dfnorm.boxplot(rot=90)
+def normalised_boxplot(df):
+    x = df.select_dtypes(include='number').values
+    scaler = pp.MinMaxScaler()
+    x_scaled = scaler.fit_transform(x)
+    dfnorm = pd.DataFrame(x_scaled,columns = df.select_dtypes(include='number').columns)
+    dfnorm.boxplot(rot=90)
+    plt.show()
 
-corrmat_big = correlation_matrix
-corrmat_big[np.absolute(corrmat_big.values) < 0.1] = 0
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+def large_correlations(df):
+    corrmat_big = correlation_matrix
+    corrmat_big[np.absolute(corrmat_big.values) < 0.1] = 0
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+    plt.show()
 
-fig, ([ax1, ax2, ax3],[ax4,ax5,ax6]) = plt.subplots(2, 3)
-ax1.scatter(df['valence'],df['danceability'],s=1)
-ax1.set_xlabel('valence')
-ax1.set_ylabel('danceability')
-ax2.scatter(df['speechiness'],df['danceability'],s=1)
-ax2.set_xlabel('speechiness')
-ax2.set_ylabel('danceability')
-ax3.scatter(df['loudness'],df['energy'],s=1)
-ax3.set_xlabel('loudness')
-ax3.set_ylabel('energy')
-ax4.scatter(df['liveness'],df['energy'],s=1)
-ax4.set_xlabel('liveness')
-ax4.set_ylabel('energy')
-ax5.scatter(df['valence'],df['energy'],s=1)
-ax5.set_xlabel('valence')
-ax5.set_ylabel('energy')
-ax6.scatter(df['tempo'],df['energy'],s=1)
-ax6.set_xlabel('tempo')
-ax6.set_ylabel('energy')
+def highest_correlators(df):
+    fig, ([ax1, ax2, ax3],[ax4,ax5,ax6]) = plt.subplots(2, 3)
+    ax1.scatter(df['valence'],df['danceability'],s=1)
+    ax1.set_xlabel('valence')
+    ax1.set_ylabel('danceability')
+    ax2.scatter(df['speechiness'],df['danceability'],s=1)
+    ax2.set_xlabel('speechiness')
+    ax2.set_ylabel('danceability')
+    ax3.scatter(df['loudness'],df['energy'],s=1)
+    ax3.set_xlabel('loudness')
+    ax3.set_ylabel('energy')
+    ax4.scatter(df['liveness'],df['energy'],s=1)
+    ax4.set_xlabel('liveness')
+    ax4.set_ylabel('energy')
+    ax5.scatter(df['valence'],df['energy'],s=1)
+    ax5.set_xlabel('valence')
+    ax5.set_ylabel('energy')
+    ax6.scatter(df['tempo'],df['energy'],s=1)
+    ax6.set_xlabel('tempo')
+    ax6.set_ylabel('energy')
+    plt.show()
 
-#test
+# call functions here, for example:
+highest_correlators(df)
 
 
